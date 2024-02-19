3d64b946ef1cee8a02f504671e152ce875f05279
B4YR1FF3R <siravit.47k@gmail.com>
2024-02-19 21:41:42 +0700

    How to play by 0852

---
+++ how to play/how to play.h
@@ -0,0 +1,143 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+//
+//                  !!!!!!!!!!   CAUTION: DO NOT TEST IN .h   !!!!!!!!!!
+//
+
+// Town Tutorial
+void Help_Choose_Town(){
+    cout << "                  Travel Route" << endl;
+    cout << "\n";
+    cout << "WARNING: Be careful!, your decision affect to your car" << endl;
+    cout << "and your characteers." << endl;
+    cout << "\n";
+    cout << "It might damaged your car or maybe kill someone." << endl;
+    cout << "            --- ENTER TO CONTINUE --- \n";
+    cin.get();
+}
+
+// Books Abilities TutoriaL
+void Help_Learning(){
+    cout << "                    Learning" << endl;
+    cout << "\n";
+    cout << "When you read the books, the books you read will" << endl;
+    cout << "improve that skills of characters who read." << endl;
+    cout << "            --- ENTER TO CONTINUE --- \n";
+    cin.get();
+}
+
+// Consuming Tutorial
+void Help_Eat(){
+    cout << "                   Consuming" << endl;
+    cout << "\n";
+    cout << "Each food you eat will increase your hunger stats" << endl;
+    cout << "differently. Depends on who eat it." << endl;
+    cout << "Choose it according to the situation." << endl;
+    cout << "            --- ENTER TO CONTINUE --- \n";
+    cin.get();
+}
+
+// Crafting Tutorial
+void Help_Crafting(){
+    cout << "                    Crafting" << endl;
+    cout << "\n";
+    cout << "An individual's crafting ability directly affects" << endl;
+    cout << "the state of the craftable items." << endl;
+    cout << "Choose the right person to craft various equipment." << endl;
+    cout << "            --- ENTER TO CONTINUE --- \n";
+    cin.get();
+}
+
+// Driving Tutorial
+void Help_Traveling(){
+    cout << "                    Traveling" << endl;
+    cout << "\n";
+    cout << "The faster you drive, The faster you arrive." << endl;
+    cout << "But this comes with increased fuel consumption as well." << endl;
+    cout << "Choose the speed according to the situation." << endl;
+    cout << "             --- ENTER TO CONTINUE --- \n";
+    cin.get();
+}
+
+// Hunting Tutorail
+void Help_Hunting(){
+    cout << "                      Hunting" << endl;
+    cout << "\n";
+    cout << "Ability of Combat and Survival will use in here." << endl;
+    cout << "The more your character have. The more opportunities you have." << endl;
+    cout << "Depends on, how much does each characrters have." << endl;
+    cout << "             --- ENTER TO CONTINUE --- \n";
+    cin.get();
+}
+
+// Jobs Tutorial
+void Help_Job(){
+    cout << "                  Accepting Jobs" << endl;
+    cout << "\n";
+    cout << "If you done some job. Hired person will pay you." << endl;
+    cout << "Depends on how difficult it is, but if you failed" << endl;
+    cout << "Hired person won't pay you any." << endl;
+    cout << "Whether you failed or win, The time passed by 1 hour" << endl;
+    cout << "per job you accept." << endl;
+    cout << "            --- ENTER TO CONTINUE --- \n";
+    cin.get();
+}
+
+// Trading Tutorial
+void Help_Trading(){
+    cout << "                      Trading" << endl;
+    cout << "\n";
+    cout << "Whether you accept the offer from trader or not." << endl;
+    cout << "The time passed by 1 hour per person who come to trade" << endl;
+    cout << "            --- ENTER TO CONTINUE --- \n";
+    cin.get();
+}
+
+// Warzone Driveby Tutorial
+void Help_WarzoneDriveby(){
+    cout << "                      Warzone" << endl;
+    cout << "\n";
+    cout << "If you travel into warzone, this decition will appear." << endl;
+    cout << "If you choose to roundabout. You're certainly safe." << endl;
+    cout << "But lost more gasolin than normal route." << endl;
+    cout << "\n";
+    cout << "If you choose to trespassing. You're in danger." << endl;
+    cout << "But you can safe gasolin more than roundabout." << endl;
+    cout << "\n";
+    cout << "If you choose to wait. The time passed by 2 hours" << endl;
+    cout << "per 1 time waiting." << endl;
+    cout << "\n";
+    cout << "Choose carefully, some choice might kill someone" << endl;
+    cout << "            --- ENTER TO CONTINUE --- \n";
+    cin.get();
+}
+
+// Stress Tutorial
+void Help_Stress(){
+    cout << "                     Stress" << endl;
+    cout << "\n";
+    cout << "The more you stress, The more you loss health" << endl;
+    cout << "You can relax by listened to music, " << endl;
+    cout << "play instrument, relax. If the character is" << endl;
+    cout << "relaxing. He/She can't do nothing unless you wake" << endl;
+    cout << "him/her. Don't be too panic." << endl;
+    cout << "If some character get too much stress." << endl;
+    cout << "They will end up unpeacefully" << endl;
+    cout << "            --- ENTER TO CONTINUE --- \n";
+    cin.get();
+}
+
+// Repair Car Tutorial
+void Help_RepairCar(){
+    cout << "                 Repairing Car" << endl;
+    cout << "\n";
+    cout << "If your car get some damage, you can fix it." << endl;
+    cout << "But if your car's out of limit. It'll explode," << endl;
+    cout << "or lucky just undrivable. You have to walk." << endl;
+    cout << "Be careful about your trail you traveling." << endl;
+    cout << "            --- ENTER TO CONTINUE --- \n";
+    cin.get();
+}
