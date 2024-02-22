700add58e48158f6a7be99c1d2cf31859a89269c
B4YR1FF3R <siravit.47k@gmail.com>
2024-02-22 21:49:53 +0700

    ending by 0852

---
+++ ending/ending.h
@@ -0,0 +1,87 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+//
+//                  !!!!!!!!!!   CAUTION: DO NOT TEST IN .h   !!!!!!!!!!
+//
+
+//                  !!!!!!!!!!   YOU MUST READ OUR STORY CHART FIRST   !!!!!!!!!
+
+// Ending1: All survival alive
+void Ending1(){
+    cout << "All of your group survived." << endl;
+    cout << "They're living peacefully in OreLand." << endl;
+    cout << "Paul become a professional guitarist." << endl;
+    cout << "He's a teacher of musical in college." << endl;
+    cout << "Ramona become a head chef with Paula." << endl;
+    cout << "They're open a little restaurant in." << endl;
+    cout << "local town. While Alejandro and Hugo" << endl;
+    cout << "are having a good moment as a mechanic." << endl;
+    cout << "They repair cars in capital city." << endl;
+    cout << "\n";
+    cout << "      --- ENTER TO CONTINUE --- \n";
+    cin.get();
+    cout << "5 years later. Alejandro passed by." << endl;
+    cout << "In his funeral, There's a lot of" << endl;
+    cout << "high ranking general come to visit him." << endl;
+    cout << "After funeral, Paul found a music tape" << endl;
+    cout << "in Alejandro's bedroom. there's a note" << endl;
+    cout << "attached in the box. it's say" << endl;
+    cout << "From Then Till Now" << endl;
+    cout << "\n";
+    cout << "      --- ENTER TO CONTINUE --- \n";
+    cin.get();
+
+    // ภาพ Paul กดเทปเล่นเพลงขึ้น
+    // เพลงขึ้น
+};
+
+// Ending2: Some survive, Some not.
+void Ending2(){
+    cout << "Finally, your group has reached to the" << endl;
+    cout << "end of insanity trail. Someone are standing" << endl;
+    cout << "taking deep soulless breath day by day." << endl;
+    cout << "Someone are having a good time on their" << endl;
+    cout << "Your group made a full of love and respect" << endl;
+    cout << "funeral. The flowers was given to dead man" << endl;
+    cout << "A lot of lilies was placed in front of the" << endl;
+    cout << "coffin. Someguys noticed a tape in corpse's" << endl;
+    cout << "body with a note attached. it's say" << endl;
+    cout << "From Then Till Now" << endl;
+    cout << "\n";
+    cout << "      --- ENTER TO CONTINUE --- \n";
+    cin.get();
+
+    
+    // ภาพวิทยุ Paul เล่นเทปเพลงขึ้น
+    // เพลงขึ้น
+};
+
+// Ending3: No Life
+void Ending3(){
+    cout << "There's no survival. A soldier found" << endl;
+    cout << "some of family's corpses. Some of them" << endl;
+    cout << "still warm." << endl;
+    cout << "\n";
+    cout << "      --- ENTER TO CONTINUE --- \n";
+    cin.get();
+    cout << "Lustful soldier drag your sister to a room." << endl;
+    cout << "Made your sister filthy with dirty white stuff." << endl;
+    cout << "\n";
+    cout << "      --- ENTER TO CONTINUE --- \n";
+    cin.get();
+    cout << "All of corpses are" << endl;
+    cout << "Transfer to meat, and sold by soldier" << endl;
+    cout << "to starving people. All equipment was sold." << endl;
+    cout << "Include with Paul's Cassette player." << endl;
+    cout << "The buyer noticed a note in it." << endl;
+    cout << "it's say";
+    cout << "From Then Till Now" << endl;
+    cout << "\n";
+    cout << "      --- ENTER TO CONTINUE --- \n";
+    cin.get();
+
+    // ภาพวิทยุ Paul เล่นเทปเพลงขึ้น
+    // เพลงขึ้น
+};
