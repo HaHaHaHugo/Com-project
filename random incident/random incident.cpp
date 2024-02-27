4f003a9c1209647de58ace333d0e8f5b3a05465a
B4YR1FF3R <siravit.47k@gmail.com>
2024-02-27 22:24:27 +0700

    random incident by 0852

--- random incident/random incident.cpp
+++ random incident/random incident.cpp
@@ -22,94 +22,94 @@ void RI2(){
 };
 
 void RI3(){
-    cout << "Hugo was sick";
+    cout << "Hugo was sick.";
     health -= 10;
 };
 
 void RI4(){
-    cout << "Paula was sick";
+    cout << "Paula was sick.";
     health -= 10;
 };
 
 void RI5(){
-    cout << "Alejandro was sick";
+    cout << "Alejandro was sick.";
     health -= 10;
 };
 
 // Random Incident 6 - 10: Depressed
 void RI6(){
-    cout << "Paul was depressed";
+    cout << "Paul was depressed.";
     stress += 10;
 };
 
 void RI7(){
-    cout << "Ramona was depressed";
+    cout << "Ramona was depressed.";
     stress += 10;
 };
 
 void RI8(){
-    cout << "Hugo was depressed";
+    cout << "Hugo was depressed.";
     stress += 10;
 };
 
 void RI9(){
-    cout << "Paula was depressed";
+    cout << "Paula was depressed.";
     stress += 10;
 };
 
 void RI10(){
-    cout << "Alejandro was depressed";
+    cout << "Alejandro was depressed.";
     stress += 10;
 };
 
 // Random Incident 11 - 15: Hungry
 void RI11(){
-    cout << "Paul was hungry";
+    cout << "Paul was hungry.";
     hunger += 10;
 };
 
 void RI12(){
-    cout << "Ramona was hungry";
+    cout << "Ramona was hungry.";
     hunger += 10;
 };
 
 void RI13(){
-    cout << "Hugo was hungry";
+    cout << "Hugo was hungry.";
     hunger += 10;
 };
 
 void RI14(){
-    cout << "Paula was hungry";
+    cout << "Paula was hungry.";
     hunger += 10;
 };
 
 void RI15(){
-    cout << "Alejandro was hungry";
+    cout << "Alejandro was hungry.";
     hunger += 10;
 };
 
 // Random Incident 16 - 20: Tired
 void RI16(){
-    cout << "Paul was tired";
+    cout << "Paul was tired.";
     stamina -= 10;
 };
 
 void RI17(){
-    cout << "Ramona was tired";
+    cout << "Ramona was tired.";
     stamina -= 10;
 };
 
 void RI18(){
-    cout << "Hugo was tired";
+    cout << "Hugo was tired.";
     stamina -= 10;
 };
 
 void RI19(){
-    cout << "Paula was tired";
+    cout << "Paula was tired.";
     stamina -= 10;
 };
 
 void RI20(){
-    cout << "Alejandro was tired";
+    cout << "Alejandro was tired.";
     stamina -= 10;
 };
