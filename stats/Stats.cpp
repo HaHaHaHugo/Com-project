ea6d81d8cd8e9f1553afd8d6ea82e994c6f5e61c
B4YR1FF3R <siravit.47k@gmail.com>
2024-02-28 11:50:23 +0700

    stats by 0852

--- stats/Stats.cpp
+++ stats/Stats.cpp
@@ -32,6 +32,20 @@ struct stats
         void abilities_max();
 };
 
+// enemy stats
+struct enemy_stats
+{
+    public:
+
+        // enemy_stats
+        string enemy_name;
+        int enemy_health;
+        int enemy_attack;
+
+        // function
+        void showEnemyStats();
+};
+
 // limit of stats
 struct stats_max
 {
@@ -83,6 +97,16 @@ void stats::showAbilities(){
     cout << "\n";
 };
 
+// show enemy stats
+void enemy_stats::showEnemyStats(){
+    cout << "Name: " << enemy_name << endl;
+    cout << "Health: " << enemy_health << endl;
+    cout << "Damage: " << enemy_attack << endl;
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+};
+
 // Test ShowStats
 int main()
 {
@@ -92,6 +116,9 @@ int main()
     stats Hugo = {"Hugo",100,0,100,100,90,70,60,0};
     stats Paula = {"Paula",50,0,50,100,40,0,10,90};
     stats Alejandro = {"Alejandro",70,0,70,100,60,80,80,0};
+
+    enemy_stats Deer = {"Deer",50,0};
+    enemy_stats Bear = {"Bear",50,20};
 
     // stats
     Paul.showStats();
@@ -106,5 +133,9 @@ int main()
     Hugo.showAbilities();
     Paula.showAbilities();
     Alejandro.showAbilities();
+
+    // enemy stats
+    Deer.showEnemyStats();
+    Bear.showEnemyStats();
 
 }
--- stats/Stats.h
+++ stats/Stats.h
@@ -36,6 +36,20 @@ struct stats
         void abilities_max();
 };
 
+// enemy stats
+struct enemy_stats
+{
+    public:
+
+        // enemy_stats
+        string enemy_name;
+        int enemy_health;
+        int enemy_attack;
+
+        // function
+        void showEnemyStats();
+};
+
 // limit of enemy stats
 struct enemy_stats_max
 {
@@ -87,6 +101,16 @@ void stats::showAbilities(){
     cout << "\n";
 };
 
+// show enemy_stats
+void enemy_stats::showEnemyStats(){
+    cout << "Name: " << enemy_name << endl;
+    cout << "Health: " << enemy_health << endl;
+    cout << "Damage: " << enemy_attack << endl;
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+};
+
 // Test ShowStats
 //
 //          !!!!!!!!!!   CAUTION : DO NOT TEST IN .h     !!!!!!!!!!
@@ -99,6 +123,9 @@ int main()
     stats Hugo = {"Hugo",100,0,100,100,90,70,60,0};
     stats Paula = {"Paula",50,0,50,100,40,0,10,90};
     stats Alejandro = {"Alejandro",70,0,70,100,60,80,80,0};
+
+    enemy_stats Deer = {"Deer",50,0};
+    enemy_stats Bear = {"Bear",50,20};
 
     // stats
     Paul.showStats();
@@ -113,5 +140,9 @@ int main()
     Hugo.showAbilities();
     Paula.showAbilities();
     Alejandro.showAbilities();
+
+    // enemy stats
+    Deer.showEnemyStats();
+    Bear.showEnemyStats();
 
 }
