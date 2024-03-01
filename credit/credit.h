cc76764d223bb0e69f0cb11fef8cf4da343fe193
B4YR1FF3R <siravit.47k@gmail.com>
2024-03-01 14:48:33 +0700

    Credit by 0852

---
+++ credit/credit.h
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+//
+//                  !!!!!!!!!!   CAUTION: DO NOT TEST IN .h   !!!!!!!!!!
+//
+
+// Credit1: Graphic Designer
+void Credit1(){
+    cout << "   Graphic Design" << endl;
+    cout << "\n";
+    cout << "Pattanit Buatip" << endl;
+    cout << "Wiriya Khotchahin" << endl;
+    cout << "Phurinat Wongkhankaew" << endl;
+    cout << "\n";
+};
+
+// Credit2: Music and Sound Director
+void Credit2(){
+    cout << "Music and Sound Director" << endl;
+    cout << "\n";
+    cout << "Siravit Kaewmakoon" << endl;
+    cout << "\n";
+};
+
+// Credit3: User Interface
+void Credit3(){
+    cout << "   User Interface" << endl;
+    cout << "\n";
+    cout << "Wichaiyut KiangKham" << endl;
+    cout << "\n";
+};
+
+// Credit4: Story
+void Credit4(){
+    cout << "       Story" << endl;
+    cout << "\n";
+    cout << "Siravit Kaewmakoon" << endl;
+    cout << "\n";
+};
+
+// Credit5: Game Developer
+void Credit5(){
+    cout << "   Game Developer" << endl;
+    cout << "\n";
+    cout << "Navapol Naksen" << endl;
+    cout << "Pattanit Buatip" << endl;
+    cout << "Phurinat Wongkhankaew" << endl;
+    cout << "Wiriya Khotchahin" << endl;
+    cout << "\n";
+};
+
+// Credit6: Game Designer
+void Credit6(){
+    cout << "   Game Designer" << endl;
+    cout << "\n";
+    cout << "Siravit Kaewmakoon" << endl;
+    cout << "\n";
+};
